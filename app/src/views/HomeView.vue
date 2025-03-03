2994f9c7a9bd57b084057729b48cdaf7843636c3
Nathank4242 <nkweon@gmail.com>
2025-03-03 13:49:14 -0500

    open

---
+++ app/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+async function GetGrad() {
+  let res = await fetch('https://data.cityofnewyork.us/resource/ynqa-y42e.json')
+  let data = await res.json()
+  graduation.value = data.results
+  console.log(data.results)
+}
+</script>
+
+<template>
+  <main></main>
+</template>
