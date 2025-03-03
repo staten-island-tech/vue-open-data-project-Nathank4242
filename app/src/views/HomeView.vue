6d6b627964cb1bf5b9edfbe1e3c77dfae97d0ad2
Nathank4242 <nkweon@gmail.com>
2025-03-03 13:53:02 -0500

    open

--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -1,12 +1,33 @@
 <script setup>
+import { ref, onMounted } from 'vue'
+
+const graduation = ref([])
+
 async function GetGrad() {
   let res = await fetch('https://data.cityofnewyork.us/resource/ynqa-y42e.json')
   let data = await res.json()
-  graduation.value = data.results
-  console.log(data.results)
+  graduation.value = data
+  console.log(data)
 }
+onMounted(() => {
+  GetGrad()
+})
 </script>
 
 <template>
-  <main></main>
+  <main>
+    <div v-if="graduation.length > 0">
+      <h1>Graduation Data</h1>
+      <ul>
+        <li v-for="(item, index) in graduation" :key="index">
+          {{ item.school_name }} - {{ item.graduation_rate }}
+        </li>
+      </ul>
+    </div>
+    <div v-else>
+      <p>Loading data...</p>
+    </div>
+  </main>
 </template>
+
+<style scoped></style>
