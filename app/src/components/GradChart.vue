fdb1caf6e3c65cdc1264f620650e8484310e6e6a
Nathank4242 <nkweon@gmail.com>
2025-03-10 14:01:42 -0400

    a

--- app/src/components/GradChart.vue
+++ app/src/components/GradChart.vue
@@ -1,16 +1,36 @@
 <template>
-  <div></div>
+  <script src="https://data.cityofnewyork.us/resource/ynqa-y42e.json"></script>
+  <body>
+    <canvas id="myChart" style="width: 100%; max-width: 600px"></canvas>
+
+    <script>
+      var xValues = [item.borough]
+      var yValues = [item.total_cohort]
+      var barColors = ['red', 'green', 'blue', 'orange', 'brown']
+
+      new Chart('myChart', {
+        type: 'bar',
+        data: {
+          labels: xValues,
+          datasets: [
+            {
+              backgroundColor: barColors,
+              data: yValues,
+            },
+          ],
+        },
+        options: {
+          legend: { display: false },
+          title: {
+            display: true,
+            text: 'World Wine Production 2018',
+          },
+        },
+      })
+    </script>
+  </body>
 </template>
 
 <script setup></script>
 
 <style lang="scss" scoped></style>
-
-const config = { type: 'bar', data: data, options: { scales: { y: { beginAtZero: true } } }, };
-const config = { type: 'bar', data: data, options: { scales: { y: { beginAtZero: true } } }, };
-const labels = Utils.months({count: 7}); const data = { labels: labels, datasets: [{ label: 'My
-First Dataset', data: [65, 59, 80, 81, 56, 55, 40], backgroundColor: [ 'rgba(255, 99, 132, 0.2)',
-'rgba(255, 159, 64, 0.2)', 'rgba(255, 205, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(54, 162, 235,
-0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(201, 203, 207, 0.2)' ], borderColor: [ 'rgb(255, 99, 132)',
-'rgb(255, 159, 64)', 'rgb(255, 205, 86)', 'rgb(75, 192, 192)', 'rgb(54, 162, 235)', 'rgb(153, 102,
-255)', 'rgb(201, 203, 207)' ], borderWidth: 1 }] };
--- app/src/views/AboutView.vue
+++ app/src/views/AboutView.vue
@@ -1,15 +1,11 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>{{ GradChart }}</h1>
   </div>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<script setup>
+import GradChart from '../components/GradChart.vue'
+</script>
+
+<style></style>
